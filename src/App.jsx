eb6edc8c769680ad29ab8c6fa97d16be1616f0b3
alanblee <alanbenlee12@gmail.com>
2020-03-27 08:10:40 -0700

    installed dependencies and structured folders

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/navbar/navbar.component";
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <NavBar />
+    </div>
+  );
+};
+export default App;
