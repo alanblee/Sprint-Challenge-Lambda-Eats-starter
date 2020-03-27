fd975135b9121b4337932be54212fee8eb512f14
alanblee <alanbenlee12@gmail.com>
2020-03-27 12:10:18 -0700

    added modal

--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,11 +1,44 @@
 import React, { useState, useEffect } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+
 import axios from "axios";
 import * as yup from "yup";
 import { formSchema } from "./formSchema";
 import { sizeInputs, sauceInputs, toppingInputs } from "./formInputs";
 import "./form.css";
 
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: 'scroll'
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    width: "50%"
+  }
+}));
+
 const Form = () => {
+  //modal
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  //form states
   const [formValues, setFormValues] = useState({
     orderFor: "",
     size: "X-Small",
@@ -124,7 +157,105 @@ const Form = () => {
   };
   return (
     <div className="form-page">
-      <h3>Build your pizza</h3>
+      <button type="button" onClick={handleOpen}>
+        click for pizza
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h3>Build your pizza</h3>
+            <form className="form-wrapper" onSubmit={handleSubmit}>
+              <label htmlFor="orderFor">
+                Order for
+                <input
+                  data-cy="orderFor"
+                  type="text"
+                  name="orderFor"
+                  value={formValues.orderFor}
+                  onChange={handleChange}
+                  placeholder="Provide a name for order"
+                />
+                {errors["orderFor"].length > 0 ? (
+                  <p data-cy={`error-orderFor`} className="error">
+                    {errors["orderFor"]}
+                  </p>
+                ) : null}
+              </label>
+
+              <label htmlFor="size">
+                <span className="span-select">
+                  Choose a size
+                  <br />
+                </span>
+                <span className="required">Required</span>
+
+                <select
+                  name="size"
+                  id="size"
+                  onChange={handleChange}
+                  data-cy="size"
+                >
+                  {renderSelectOptions()}
+                </select>
+              </label>
+
+              <label htmlFor="sauce" className="sauce">
+                <span className="span-select">
+                  Choice of Sauce
+                  <br />
+                </span>
+                <span className="required">Required</span>
+
+                {renderRadioOptions()}
+              </label>
+
+              <label htmlFor="toppings" className="topping">
+                <span className="span-select">
+                  Add Toppings
+                  <br />
+                </span>
+                <span className="required">Pro tip: choose all of them</span>
+                {renderCheckbox()}
+              </label>
+
+              <label htmlFor="specialInstructions">
+                Special Instructions
+                <input
+                  type="textarea"
+                  name="specialInstructions"
+                  value={formValues.specialInstructions}
+                  onChange={handleChange}
+                  placeholder="Anything else you would like to add?"
+                  data-cy="specialInstructions"
+                />
+                {errors["specialInstructions"].length > 0 ? (
+                  <p data-cy={`error-specialInstructions`} className="error">
+                    {errors["specialInstructions"]}
+                  </p>
+                ) : null}
+              </label>
+
+              <div className="btn-container">
+                <button disabled={buttonDisabled} data-cy="form-btn">
+                  Add to order
+                </button>
+              </div>
+            </form>
+          </div>
+        </Fade>
+      </Modal>
+      {/* <h3>Build your pizza</h3>
       <form className="form-wrapper" onSubmit={handleSubmit}>
         <label htmlFor="orderFor">
           Order for
@@ -196,7 +327,7 @@ const Form = () => {
             Add to order
           </button>
         </div>
-      </form>
+      </form> */}
       <pre>{JSON.stringify(pizza, null, 2)}</pre>
     </div>
   );
